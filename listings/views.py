0701db0d7d3018cb47d03fed5307bb1468200629
mayuri238 <mhaskemayuri@12gmail.com>
2025-06-28 23:56:58 +0530

    Fix circular import of property_detail

--- listings/views.py
+++ listings/views.py
@@ -10,8 +10,6 @@ from .models import Property
 from django.db.models import Q
 from django.core.mail import send_mail
 from django.conf import settings
-
-
 from .forms import ContactForm
 
 def contact(request):
