af45ff051835d1e1200228d73b69fb6e2f8b5147
mayuri238 <mhaskemayuri@12gmail.com>
2025-07-03 15:09:56 +0530

    Fix templates DIRS to include findnest/templates for login page

--- findnest/settings.py
+++ findnest/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'findnest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'findnest', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
